7502ac4c7c16716da300f072157670d642ff74d9
amish <maamish8765@gmail.com>
2023-05-10 19:30:33 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(title: Text("hello")),
+          appBar: AppBar(title: Text("helloooo")),
 
         ), );
     }
