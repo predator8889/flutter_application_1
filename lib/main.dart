e94f47a9a3e51a64ddb6b052eab61fa37edba30d
amish <maamish8765@gmail.com>
2023-05-10 19:31:47 +0500

    fff

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(title: Text("helloooo")),
+          appBar: AppBar(title: Text("helloooooooooo")),
 
         ), );
     }
